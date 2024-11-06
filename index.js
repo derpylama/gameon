26c83f80ead970b348a0b7e379b2b791d949751e
derpylama <larssonemil55@gmail.com>
2024-11-06 11:26:33 +0100

    started on collisions

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ document.addEventListener("keyup", (e) =>{
     }
 });
 
-class Gameobj{
+class Wall{
     constructor(x, y, width, height, img,){
         this.x = x;
         this.y = y;
@@ -55,32 +55,79 @@ class Gameobj{
         this.img = img;
         this.vY = 0;
         this.vX = 0;
+        this.dx = 0;
+        this.dy = 0;
+        this.collidedY = false;
+        this.collidedX = false;
     }
+    
+    detection(){
+        var collisionDetectedX = false;
+        var collisionDetectedY = false;
+        
+        
+        
+            let element = player;
+            
+            if (this != element) {
+                    collisionDetectedX =  
+                    this.x + this.width > element.x && 
+                    this.x < element.x + element.width;
+                    
+                    collisionDetectedY = 
+                    this.y + this.height > element.y && 
+                    this.y < element.y + element.height;               
+                    
+                    if(collisionDetectedY){
+
+
+
+                        /*objList.forEach((e) => {    
+                            
+                            
+                            
+                            if(e != this){
+                                e.dy = 0;
+                                e.vY = 0;
+
+                            }
+                                
+                                e.collidedY = true;
+                            
+                                if(element.y < this.y)
+                                    this.y = element.y + element.height;
+
+                                if(element.y > this.y)
+                                    this.y = element.y - this.height;
+                            
+                            
+                        })*/ 
+
+                    }
+                    
+                }
+               
+        }
+
 
     move(){
 
         var angle = player.currentRot * Math.PI/180;
-        var dx;
-        var dy;
 
+        if(!this.collidedY){
+            this.dy = speed * Math.sin(angle);  
+        }
 
-        if(angle != 0){
-            dx = speed * Math.cos(angle);
-            dy = speed * Math.sin(angle);
-        }
-        else{
-            dy = 1;
-            dx = 0;
-        }
+        this.dx = speed * Math.cos(angle);
 
         if(moveUp){
-            this.vY = -dy;
-            this.vX = -dx
+            this.vY = this.dy;
+            this.vX = this.dx
         }
 
         if(moveDown){
-            this.vY = dy;
-            this.vX = dx;
+            this.vY = -this.dy;
+            this.vX = this.dx;
         }
 
 
@@ -88,7 +135,7 @@ class Gameobj{
             this.y += this.vY;
             this.x += this.vX;
         }
-    }   
+    }; 
 
     render(){
         ctx.fillStyle = "white";
@@ -120,9 +167,9 @@ class Player {
         ctx.translate(this.x + this.width/2, this.y + this.height/2);
         ctx.rotate(this.currentRot * Math.PI / 180);
         ctx.fillStyle = "white";
-        ctx.fillRect(-this.width / 2 - 5, -this.height / 2 - 5, this.width + 10, this.height + 10)
+        ctx.fillRect(-this.width / 2 - 5, -this.height / 2 - 5, this.width + 10, this.height + 10);
         ctx.fillStyle = "black";
-        ctx.fillRect(-this.width / 2, -this.height / 2, this.width, this.height) 
+        ctx.fillRect(-this.width / 2, -this.height / 2, this.width, this.height);
         ctx.restore();
     }
 
@@ -139,15 +186,18 @@ class Player {
             this.rotation = 0;
         }
     }
+
 }
 
-var player = new Player(490,290,20,20,"",1) 
+var player = new Player(490,290,20,20,"") 
 
 visibleObjList = [];
 
+
 objList = [];
-objList.push(new Gameobj(100,100,500,60,""))
-objList.push(new Gameobj(100,500,500,60,""))
+objList.push(new Wall(100,100,500,60,""))
+objList.push(new Wall(100,400,500,60,""))
+
 
 
 function gameLoop (){
@@ -156,14 +206,18 @@ function gameLoop (){
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    
     player.update();
-
-    player.rotateRender();
-
+    
     objList.forEach(element => {
         element.move();
     });
-
+    
+    objList.forEach((element) => {
+        element.detection();
+    })
+    
+    player.rotateRender();
 
     objList.forEach(element => {
         element.render();
